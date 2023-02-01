ce606d90e5b94e0a5226b77550fa10b83a7051d3
sigmu92 <maxwellw92@gmail.com>
2023-02-01 07:53:58 -0800

    Add javascript to validate matching passwords

--- javascript.js
+++ javascript.js
@@ -1,12 +1,27 @@
 const passwordInvalid = document.querySelector(".invalid-pass");
 const passwordInputs = document.querySelectorAll(".password");
 
-window.addEventListener("load",checkValid())
-console.log(passwordInputs[0])
+
+window.addEventListener("onload",checkValid())
+passwordInputs.forEach(password => password.addEventListener('input', checkValid))
 
 function checkValid(){
-    if (passwordInputs[0]['textContent']==""){
-        passwordInputs[0]['validity']['valid'] = false;
+    if ((passwordInputs[0]['value'] == "") ||
+        (passwordInputs[0]['value'] != passwordInputs[1]['value'])){
+        
+        setInvalid()
+    } else {
+        removeInvalid()
     }
+}
+
+function setInvalid(){
+    passwordInputs.forEach(password => password.classList.add("invalid"))
+    passwordInvalid.style.color = 'red';
+}
+
+function removeInvalid(){
+    passwordInputs.forEach(password => password.classList.remove('invalid'))
+    passwordInvalid.style.color = 'white';
 }
 
